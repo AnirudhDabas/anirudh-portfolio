cae8736b6afc3990f92070c3c791da0b38ecc303
Anirudh Dabas <anirudhdabas7@gmail.com>
2025-05-24 13:08:07 +0530

    Updated social links: GitHub, LinkedIn, Email

--- src/components/global/Socials.tsx
+++ src/components/global/Socials.tsx
@@ -12,17 +12,17 @@ const Socials: FC<SocialsProps> = props => {
         {
             aria: 'Linkedin',
             icon: <FiLinkedin />,
-            href: 'https://linkedin.com/in/jasonfyw/',
+            href: 'https://www.linkedin.com/in/anirudhdabas',
         },
         {
             aria: 'GitHub',
             icon: <FiGithub />,
-            href: 'https://github.com/jasonfyw',
+            href: 'https://github.com/AnirudhDabas',
         },
         {
             aria: 'Email',
             icon: <FiMail />,
-            href: 'mailto:jason@jasonfyw.com',
+            href: 'mailto:anirudhdabas@gmail.com',
         },
     ]
     return (
